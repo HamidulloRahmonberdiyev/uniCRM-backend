b3fb38cfaef2816c0a38ee3fef53e18010dc126b
Hamidullo Rahmonberdiyev <hamidullo0760@gmail.com>
2025-04-06 15:05:49 +0500

    Bugs fix

--- app/Services/Order/OrderService.php
+++ app/Services/Order/OrderService.php
@@ -20,7 +20,7 @@ class OrderService
         ]);
 
         return Order::query()
-            ->select(['id', 'customer_id', 'user_id', 'company_id', 'source_id', 'date', 'status', 'sum', 'city_id', 'district_id', 'neighborhood_id', 'address', 'quantity', 'note', 'latitude', 'longitude'])
+            ->select(['id', 'customer_id', 'user_id', 'company_id', 'source_id', 'date', 'status', 'sum', 'city_id', 'district_id', 'neighborhood_id', 'address', 'quantity', 'note', 'latitude', 'longitude', 'created_at'])
             ->when($request->filled('status'), fn($q) => $q->where('status', $request->status))
             ->when($request->filled('start_date') && $request->filled('end_date'), function ($q) use ($request) {
                 $q->whereBetween('date', [$request->start_date, $request->end_date]);
